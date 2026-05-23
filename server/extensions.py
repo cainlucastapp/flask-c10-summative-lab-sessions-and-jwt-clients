987f2d09f22a8910e076c54c029edc5567d027a1
Cain Tapp <cainlucastapp@gmail.com>
2026-05-22 19:57:44 -0700

    server/extensions.py setup

--- server/extensions.py
+++ server/extensions.py
@@ -1,1 +1,18 @@
 #server/extensions.py
+
+from flask_sqlalchemy import SQLAlchemy
+from flask_migrate import Migrate
+from flask_bcrypt import Bcrypt
+from flask_restful import Api
+from flask_cors import CORS
+from sqlalchemy import MetaData
+
+metadata = MetaData(naming_convention={
+    "fk": "fk_%(table_name)s_%(column_0_name)s_%(referred_table_name)s",
+})
+
+db = SQLAlchemy(metadata=metadata)
+migrate = Migrate()
+bcrypt = Bcrypt()
+api = Api()
+cors = CORS()
