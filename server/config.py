c26bf8d717a3540db15b67d712bcbee600b8c929
Cain Tapp <cainlucastapp@gmail.com>
2026-05-22 19:56:59 -0700

    config.py setup

--- server/config.py
+++ server/config.py
@@ -1,2 +1,9 @@
 #server/config.py
 
+import os
+
+class Config:
+    SQLALCHEMY_DATABASE_URI = 'sqlite:///app.db'
+    SQLALCHEMY_TRACK_MODIFICATIONS = False
+    SECRET_KEY = os.environ.get('SECRET_KEY', 'dev-secret-key-change-in-production')
+    SESSION_TYPE = 'filesystem'
